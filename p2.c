86f2f64819e0f3113f277313b9dd2197f7e5cbe0
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-26 16:28:30 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	printf("Enter the temperature in celcius  :\n");
+	scanf("%d",&a);
+	b=((a*9/5)+32);
+	printf("The temperature in farenheit is :%f\n",b);
+	return 0;
+	
+}
