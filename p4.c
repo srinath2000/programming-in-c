265220f9adaccca86a4b4748e6b500a071597d70
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-29 22:50:13 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float s,u,a,t;
+	printf("Enter the velovity , acceleration and time \n");
+	scanf("%f%f%f",&u,&a,&t);
+	s=((u*t)+(0.5*a*t*t));
+	printf("the distance travelled is :%f \n",s);
+	return 0;
+
+}
