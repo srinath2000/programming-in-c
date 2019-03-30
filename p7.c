5041c41ce9627e34b42433ad818ff7090d557931
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-30 08:55:45 +0530

    Add files via upload

---
+++ p7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	
+	int num_1;
+	printf("Enter the number");
+	scanf("%d",&num_1);
+	if (num_1%7==0)
+		printf("The number is divisible by 7 :");
+	else 
+		printf(" The number is not divisible by 7 :");
+	return 0;
+	
+	}
+			
