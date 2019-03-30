8f6fcf9f28f05f573b642d34933182c429649d4c
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-30 09:12:50 +0530

    Add files via upload

---
+++ p13.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,j,i,d=1;
+	printf("Enter the lower and upper limit  :\n");
+	scanf("%d%d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+	d=0;
+	for(j=2;j<=i-1;j++)
+		{
+
+		if (i%j==0)
+		{ 
+		d=1;
+		break;
+		}
+		}
+
+	if(d==0)
+	{
+		printf("%d \n",i);
+	}
+	
+	
+	}
+	
+return 0;
+}
