7a9acad806b228909fabde245f9759d6ca326984
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-30 09:06:39 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,rem=0,rev;
+	printf("Enter a number  :\n");
+	scanf("%d",&n);
+	rev=n;
+	while(n>0)
+	{
+		sum=n%10;
+		rem=rem+(sum*sum*sum);
+		n=n/10;
+	}
+	if(rev==rem)
+	{
+		printf("The number is armstrong.... \n");
+	}
+	else 
+	{
+		printf("Not armstrong...  \n");
+	}
+	
+return 0;
+}
