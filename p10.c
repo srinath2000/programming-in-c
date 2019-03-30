68fc66e1bdb89d223d155ba692e8dbbfc9da5eda
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-30 09:04:09 +0530

    Add files via upload

---
+++ p10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,rem=0,rev;
+	printf("Enter a number  :\n");
+	scanf("%d",&n);
+	rev=n;
+	while(n>0)
+	{
+		rem=n%10;
+		sum=rem+(sum*10);
+		n=n/10;
+	}
+	
+	if(sum==rev)
+	{
+		printf("The number is palindrome.... \n");
+	}
+	else 
+	{
+		printf("Not palindrome...  \n");
+	}
+return 0;
+}
