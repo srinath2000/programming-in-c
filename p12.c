9496794c5ffde5b90e5291266369670695118acb
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-03-30 09:08:19 +0530

    Add files via upload

---
+++ p12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,d=1;
+	printf("Enter a number  :\n");
+	scanf("%d",&n);
+	for(i=2;i<n;i=i+1)
+		{
+		if (n%i==0)
+		{d=0; break;}
+		}
+	if(d==1)
+	{
+		printf("The number is prime.... \n");
+	}
+	else 
+	{
+		printf("Not prime...  \n");
+	}
+	
+	
+return 0;
+}
