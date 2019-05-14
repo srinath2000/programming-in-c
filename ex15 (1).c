e5cf102ba873675c08c1e452f766c769f632c2a3
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-05-14 21:33:15 +0530

    Add files via upload

---
+++ ex15 (1).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{ int a[100],i,small,larg,n;
+  printf("Enter total no: ");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+   scanf("%d",&a[i]);
+  small=a[0];
+  larg=a[0];
+  for(i=0;i<n;i++)
+   if(a[i]<small)
+    small=a[i];
+  for(i=0;i<n;i++)
+   if(a[i]>larg)
+    larg=a[i]; 
+  printf("lar=%d\nsmall=%d",larg,small);
+}
