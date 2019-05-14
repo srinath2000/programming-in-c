246e6f520ad978e71a07b9e46cd4726b3dcd0b53
Srinath Rajan <43911105+srinath2000@users.noreply.github.com>
2019-05-14 21:14:33 +0530

    Add files via upload

---
+++ 25.c
@@ -0,0 +1,29 @@
+//search an element using pointers
+
+
+#include<stdio.h>
+void main()
+	{
+ 	int a[100],n,flag=0,s,i, *p;
+ 	p=a;
+ 	printf("Enter the number of elements:");
+ 	scanf("%d",&n);
+ 	printf("Enter the elements:-");
+ 	for( i=0;i<n;i++) 
+	{  	
+	scanf("%d",(p+i));
+	}
+ 	printf("Enter the element to be searched:");
+ 	scanf("%d",&s);
+	for( i=0;i<n;i++)
+ 	{
+  	if(s==*(p+i))
+   	flag=1;
+ 	}
+ 	if(flag==1)
+  		printf("Entered number found");
+ 	else
+  		printf("Entered number not found");
+ 
+	}
+
